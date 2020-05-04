60b95314cfd9cd88f2d16fe22791b15399ecb090
vn50cy5 <ujjwol.bhandari@walmart.com>
2020-05-04 00:32:29 -0500

    subscribe() mono instead of block()

--- lab11/qn1/src/main/java/com/sa/lab11/reactivestockclient/ReactivestockclientApplication.java
+++ lab11/qn1/src/main/java/com/sa/lab11/reactivestockclient/ReactivestockclientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ReactivestockclientApplication implements CommandLineRunner {
@@ -18,11 +19,12 @@ public class ReactivestockclientApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         for(int i=0; i++<5;) {
             Thread.sleep(2500);
-            System.out.println(WebClient.create("http://localhost:8080/stock")
+            Mono<Double> stockPrice = WebClient.create("http://localhost:8080/stock")
                     .get()
                     .retrieve()
-                    .bodyToMono(String.class)
-                    .block());
+                    .bodyToMono(Double.class);
+
+            stockPrice.subscribe(s -> System.out.println(s));
         }
     }
 }
